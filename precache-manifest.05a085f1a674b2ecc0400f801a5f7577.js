afce0227560f4b54c428160b2a9fa3d736472e52
Dima Semenov <semenovdima998@gmail.com>
2021-01-26 17:21:10 +0200

    Updates

---
+++ precache-manifest.05a085f1a674b2ecc0400f801a5f7577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11579dc87a89bc153a06fdb03065386a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22d4c2ad312ec3c6af04",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d4c2ad312ec3c6af04",
+    "url": "./static/js/main.4a750626.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
